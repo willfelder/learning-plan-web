b73d3daa815a563551c2828ec63d99c7968f09d8
William Lengenfelder <william.lengenfelder@gmail.com>
2022-10-09 07:33:07 -0300

    update app

--- learning-plan-web/src/components/footer/Footer.jsx
+++ learning-plan-web/src/components/footer/Footer.jsx
@@ -6,7 +6,6 @@ const Footer = () => {
     return(
         <footer>
             <p>Website developed by William Lengenfelder</p>
-            <a href="https://github.com/willfelder">Github</a>
         </footer>
     )
 };
